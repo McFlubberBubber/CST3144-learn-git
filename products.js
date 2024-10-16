3ee1dccbf2f7032b2227eb81e03a8ddd97494676
Nathan <nathaniel563@hotmail.com>
2024-10-16 09:33:31 +0400

    added conditions and loops + refactored code

---
+++ products.js
@@ -0,0 +1,30 @@
+//Storing the list of products to display in this seperate JS file
+let products = [
+    {
+        "id": 1001,
+        "title": "Cat Food, 25lb bag",
+        "description": "A 25 pound bag of <em>irresistible</em>," + "organic goodness for your cat.",
+        "price": 20.00,
+        "image": "images/product-fullsize.png",
+        "availableInventory": 10,
+        "rating": 3
+    },
+    {
+        "id": 1002,
+        "title": "Cat Food, 50lb bag",
+        "description": "A 50 pound bag of <em>irresistible</em>," + "organic goodness for your cat.",
+        "price": 35.00,
+        "image": "images/product-fullsize.png",
+        "availableInventory": 7,
+        "rating": 4
+    },
+    {
+        "id": 1003,
+        "title": "Dog Food, 25lb bag",
+        "description": "A 25 pound bag of <em>irresistible</em>," + "organic goodness for your dog.",
+        "price": 19.50,
+        "image": "images/product-fullsize.png",
+        "availableInventory": 10,
+        "rating": 2
+    }
+]
